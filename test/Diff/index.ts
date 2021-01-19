24fbae46cd1408d14026e0861544663f2800dc32
lianjun <lianjun>
2021-01-19 10:27:11 +0800

    Diff

---
+++ test/Diff/index.ts
@@ -0,0 +1,13 @@
+type SetDifference<T,U> = T extends U?never:T;
+type Diff<T extends object,U extends object> = Pick<T,SetDifference<keyof T,keyof U>>;
+type a = {
+    a:1,
+    b:2
+};
+type b = {
+    a:1
+}
+let config:Diff<a,b>;
+/* {
+    b:2
+} */
